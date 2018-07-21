dc40d7d1a07274cd9d2ec5c6487f6921ef4d13c7
yu.yadong <yuyadong@zgrj.net>
2018-07-21 15:26:21 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Hello world</h1>
+    <div>hahs</div>
+    <p>{{ msg }}</p>
+    <button>{{ btnTxt }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, facere? Consequuntur non inventore quibusdam? In saepe voluptates perferendis totam ad qui quaerat autem, quidem quasi culpa modi porro obcaecati ullam!",
+      btnTxt: "按钮"
+    }
+  }
+}
+</script>
